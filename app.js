ccdf3b46a73464f2d73e57a1ce668b8171c6ce60
Tanuj <tanujagrawal1432@gmail.com>
2026-01-10 00:47:29 +0530

    Portfolio

---
+++ app.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+    window.scrollTo(0, 0);
+});
+
+
+
+const animatedItems = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.25,
+    }
+);
+
+animatedItems.forEach((item) => observer.observe(item));
+
